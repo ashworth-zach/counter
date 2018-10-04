2e9db8efd6a493f856f9a187474870b9557edc61
ashworth-zach <39654996+ashworth-zach@users.noreply.github.com>
2018-10-04 13:01:56 -0500

    finished

--- server.py
+++ server.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key='secret'
-@app.route('/')
+counter = 1
+@app.route('/', methods=['get'])
 def index():
-    counter = 1
-    session['counter'] += counter
+    if session==None:
+        session['counter']=counter
+    session['counter']=session['counter'] +1
+    print(session['counter'])
     return render_template('index.html')
 
 @app.route('/add', methods=['POST'])
 def add():
     if request.form['button'] == 'counter':
-        session['counter'] += 1
+        session['counter']=session['counter']+1
     return redirect('/')
 
-@app.route('/reset', methods=['POST'])
+@app.route('/reset', methods=['GET'])
 def reset():
-    if request.form['button'] == 'reset':
-        session['counter'] = 0
+    session['counter'] = -1
     return redirect('/')
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return render_template('index.html', counter=0 )
 if __name__=="__main__":
     app.run(debug=True)
